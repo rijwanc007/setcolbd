aaa003df43c5afa2000ad732ca8ccfbfde05d9ba
rijwan <rijwanc007@gmail.com>
2020-08-12 15:54:41 +0600

    ,'code_clean'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react';
+import React,{ Component/*,Suspense, lazy*/ } from 'react';
 import {render} from 'react-dom';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
@@ -15,31 +15,82 @@ import './assets/css/responsive.css';
 import './assets/css/custom.css';
 import './assets/css/review.css';
 
-import $ from "jquery";
-
 import './assets/lib/bootstrap/js/bootstrap.min.js';
 import './assets/lib/knob/jquery.knob.js';
 import './assets/lib/nivo-slider/js/jquery.nivo.slider.js';
 import './assets/lib/appear/jquery.appear.js';
 
+import loadable from 'react-loadable';
+
 import ScrollHandling from './component/include/ScrollHandling';
-import Dashboard from './component/pages/Dashboard';
-import WebApplication from './component/pages/WebApplication';
-import MobileApplication from './component/pages/MobileApplication';
-import QATesting from './component/pages/QA-&-Testing';
-import GraphicsSolution from './component/pages/GraphicsSolution';
-import SEODigitalMarketing from './component/pages/SEO-&-DigitalMarketing';
-import OnlineDataSecurity from './component/pages/OnlineDataSecurity';
-import ERP from './component/pages/ERP';
-import Accounts from './component/pages/Accounts';
-import Inventory from './component/pages/Inventory';
-import SalesAndDistribution from './component/pages/SalesAndDistribution';
-import CRM from './component/pages/CRM';
-import HRAndPayroll from './component/pages/HRAndPayroll';
-import ECommerce from './component/pages/ECommerce';
-import BookingAndReservation from './component/pages/BookingAndReservation';
-import VirtualClassroom from './component/pages/VirtualClassroom';
-import Career from './component/pages/Career';
+const Dashboard = loadable({
+    loader : () => import('./component/pages/Dashboard'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const WebApplication = loadable({
+    loader : () => import('./component/pages/WebApplication'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const MobileApplication = loadable({
+    loader : () => import('./component/pages/MobileApplication'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const QATesting = loadable({
+    loader : () => import('./component/pages/QA-&-Testing'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const GraphicsSolution = loadable({
+    loader : () => import('./component/pages/GraphicsSolution'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const SEODigitalMarketing = loadable({
+    loader : () => import('./component/pages/SEO-&-DigitalMarketing'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const OnlineDataSecurity = loadable({
+    loader : () => import('./component/pages/OnlineDataSecurity'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const ERP = loadable({
+    loader : () => import('./component/pages/ERP'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const Accounts = loadable({
+    loader : () => import('./component/pages/Accounts'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const Inventory = loadable({
+    loader : () => import('./component/pages/Inventory'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const SalesAndDistribution = loadable({
+    loader : () => import('./component/pages/SalesAndDistribution'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const CRM = loadable({
+    loader : () => import('./component/pages/CRM'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const HRAndPayroll = loadable({
+    loader : () => import('./component/pages/HRAndPayroll'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const ECommerce = loadable({
+    loader : () => import('./component/pages/ECommerce'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const BookingAndReservation = loadable({
+    loader : () => import('./component/pages/BookingAndReservation'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const VirtualClassroom = loadable({
+    loader : () => import('./component/pages/VirtualClassroom'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
+const Career = loadable({
+    loader : () => import('./component/pages/Career'),
+    loading : () => <div className={'text-info'}> Loading...... </div>,
+});
 
 class LandingPage extends Component{
     render(){
